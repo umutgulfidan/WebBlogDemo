38e6f857dfd84167dbb016971e0eeee1bf3eed8c
umutgulfidan <umutgulfidan41@gmail.com>
2025-02-04 14:03:40 +0300

    Added Message Controller and InBox,MesageDetails actions

---
+++ CoreDemo/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager _messageManager = new MessageManager(new EfMessageRepository());
+        public IActionResult InBox()
+        {
+            var values = _messageManager.GetListByReceiver(1);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult MessageDetails(int id)
+        {
+            var value = _messageManager.GetById(id);
+            return View(value);
+        }
+    }
+}
---
+++ CoreDemo/Models/TimeHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace CoreDemo.Models
+{
+    public class TimeHelper
+    {
+        public static string CalcDifference(DateTime date)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan difference = now - date;
+            string timeLabel;
+            if (difference.Days >= 1)
+            {
+                timeLabel = $"{((int)difference.TotalDays)} gün önce ";
+            }
+            else if (difference.TotalHours >= 1)
+            {
+                timeLabel = $"{(int)difference.TotalHours} saat önce";
+            }
+            else if (difference.TotalMinutes >= 1)
+            {
+                timeLabel = $"{(int)difference.TotalMinutes} dakika önce";
+            }
+            else
+            {
+                timeLabel = "Az önce";
+            }
+            return timeLabel;
+        }
+    }
+}
