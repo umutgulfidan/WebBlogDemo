17baba7e856ae5893d027a91a00cedecdfbb2723
umutgulfidan <umutgulfidan41@gmail.com>
2025-02-08 16:56:07 +0300

    Added a static example of google chart.

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Xml.Linq;
 using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
@@ -15,6 +16,11 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = blogManager.GetList().Count;
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string api = "b7bbe5cd0d21e554541d9de330672622";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Istanbul&mode=xml&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
