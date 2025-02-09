43440bed41138a1ac81628a27eae690bf43d732e
umutgulfidan <umutgulfidan41@gmail.com>
2025-02-09 16:29:24 +0300

    Added WriterController for AJAX practice.

---
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,71 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult GetWriterByID(int writerId)
+        {
+            var findWriter = writers.FirstOrDefault(writers => writers.Id == writerId);
+            var jsonWriter = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriter);
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(w => w.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+        [HttpPost]
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(writer => writer.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(w);
+            return Json(jsonWriter);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass> 
+        { 
+            new WriterClass()
+            {
+                Id = 1,
+                Name = "Ayşe"
+            },
+            new WriterClass()
+            {
+                Id = 2,
+                Name = "Ahmet"
+            },
+            new WriterClass()
+            {
+                Id = 3,
+                Name = "Buse"
+            }
+        };
+    }
+}
