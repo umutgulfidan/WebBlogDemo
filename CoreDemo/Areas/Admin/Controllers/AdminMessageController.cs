027da46cb7e8214fc7ff6b5aa4a454ca0f1969f2
umutgulfidan <umutgulfidan41@gmail.com>
2025-02-14 15:25:31 +0300

    Added AdminMessage Controller . Added Inbox,Sendbox,ComposeMessage actions. And some visual changes

---
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        UserManager<AppUser> _userManagerIdentity;
+        MessageManager _messageManager = new MessageManager(new EfMessageRepository());
+
+        public AdminMessageController(UserManager<AppUser> userManagerIdentity)
+        {
+            _userManagerIdentity = userManagerIdentity;
+        }
+        [HttpGet]
+        public IActionResult Inbox()
+        {
+            var userId = Convert.ToInt32(_userManagerIdentity.GetUserId(User));
+            var values = _messageManager.GetListByReceiver(userId);
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult SendBox()
+        {
+            var userId = Convert.ToInt32(_userManagerIdentity.GetUserId(User));
+            var values = _messageManager.GetListBySender(userId);
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult ComposeMessage()
+        {
+            return View();
+        }
+    }
+}
