ec518824b81ccdede517b52ea547a1a4d86204b4
umutgulfidan <umutgulfidan41@gmail.com>
2025-02-15 17:48:55 +0300

    Added Role Controller for Admin Area

---
+++ CoreDemo/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,51 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+
+             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel role)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole appRole = new AppRole() { Name = role.Name};
+                var result = await _roleManager.CreateAsync(appRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("",item.Description);
+                }
+            }
+
+            return View(role);
+        }
+    }
+}
